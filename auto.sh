97a7a99ee00da6347a38c93272453ac35e9a1434
baddate <badgatex@outlook.com>
2021-01-13 16:10:19 +0800

    add some pictures via script

---
+++ auto.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# restart
+if [[ -f "output" || -f "input" ]]; then
+rm -f output input
+fi
+
+# Assign the filename
+ls > input
+
+filename="input"
+
+value="$(<$filename)"
+
+if [[ -f "output" ]]; then
+rm output
+fi
+
+for val in $value
+do
+    if [[ $val != "" && $val != "input" && $val != "output" ]]; then
+    replace="https://cdn.jsdelivr.net/gh/baddate/imagedb/$val"
+        if [[ ! -f "output" ]]; then
+        echo "$replace" > output
+        else
+        echo "$replace" | tee -a output
+        fi
+    fi
+done
